312b75214b7514cce6ca254ba0ac6e07f94ee4c8
Markel Ramiro <gernika2002@gmail.com>
2023-11-22 06:16:40 -0500

    user fetch finalizado con MongoDB

--- routes/users.js
+++ routes/users.js
@@ -1,39 +1,77 @@
 var express = require('express');
 var router = express.Router();
-
-let users = [
-  {id: Date.now(), izena: "John", abizena: "Doe", email: "john@doe.com"},
-];
+const mongojs = require('mongojs');
+const db = mongojs('usersdb', ['users']);
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render("users", {
-    title: "Users", 
-    users: users
+router.get('/', function(req, res) {
+  db.users.find( function (err, docs) {
+      if (err) {
+          console.log(err)
+      } else {
+          res.render('users', {
+              title: 'Users',
+              users: docs
+          })
+      }
+  })
+});
+
+router.post('/new', function(req, res) {
+  const bezeroBerria = {
+      izena : req.body.izena,
+      abizena: req.body.abizena,
+      email: req.body.email
+  };
+
+  db.users.insert(bezeroBerria, function(err, doc) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/users');
+    }
   });
 });
 
-router.get('/list', function(req, res, next) {
-  res.json(users)
-  });
-
-
-router.post("/new", (req, res) => {
-  users.push(req.body);
-  res.json(users);
-});
-
-router.delete("/delete/:id", (req, res) => {
-  users = users.filter(user => user.id != req.params.id);
-  res.json(users);
-});
-
-router.put("/update/:id", (req, res) => {
-  let user = users.find(user => user.id == req.params.id);
-  user.izena = req.body.izena;
-  user.abizena = req.body.abizena;
-  user.email = req.body.email;
-  res.json(users);
+router.post('/delete/', function(req, res) {
+  db.users.remove(
+      {_id:  mongojs.ObjectID(req.body._id)}, function () {
+          console.log("zuzen ezabatu da");
+      }
+  );
+  res.redirect('/users');
 })
 
+
+
+router.get('/edit/:id', function(req, res) {
+  db.users.findOne({_id: mongojs.ObjectID(req.params.id)}, function (err, doc) {
+      if (err) {
+          res.send(err);
+      } else {
+          res.render('editUser', {
+              user: doc
+          });
+      }
+  });
+});
+
+router.post('/update', function(req, res) {
+  const updatedUser = {
+      izena: req.body.izena,
+      abizena: req.body.abizena,
+      email: req.body.email
+  };
+
+  db.users.update({_id: mongojs.ObjectID(req.body._id)}, {$set: updatedUser}, function(err, doc) {
+      if (err) {
+          res.send(err);
+      } else {
+          res.redirect('/users');
+      }
+  });
+});
+
+
 module.exports = router;
+
